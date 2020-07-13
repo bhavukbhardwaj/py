4373874dec888596f4892355e8b88b72bfc4b341
Bhavuk Bhardwaj <bhavukbhardwaj03@gmail.com>
2020-07-13 14:09:43 +0530

    python project

---
+++ FlaskWebProject1/app.py
@@ -0,0 +1,40 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
+from flask import Flask, request
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+
+@app.route('/')
+def index():
+  return 'Server Works!'
+  
+@app.route('/greet')
+def say_hello():
+  return 'Hello from Server'
+
+@app.route('/user/<username>')
+def show_user(username):
+  #returns the username
+  return 'Username: %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+  #returns the post, the post_id should be an int
+  return str(post_id)
+
+
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 9020
+    app.run(HOST, PORT)
